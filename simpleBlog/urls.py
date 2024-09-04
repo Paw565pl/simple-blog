1dd57ce2503e2c8d6db3f0c449996fc151127be9
Paw565pl <paw565pl@gmail.com>
2024-09-04 16:26:39 +0200

    chore: set admin site header

--- simpleBlog/urls.py
+++ simpleBlog/urls.py
@@ -21,6 +21,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from core.views import PageNotFoundView
 
+admin.site.site_header = "Simple Blog"
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("blog.urls")),
